ed5ffcfbf313fbe1e52ae5d4d29be250b50d3f42
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-10-05 11:04:26 +0100

    MADE SOME TWEAKING

--- lib/trpc/utils.ts
+++ lib/trpc/utils.ts
@@ -1,8 +1,21 @@
-import { env } from "@/lib/env.mjs";
+// import { env } from "@/lib/env.mjs";
+// function getBaseUrl() {
+//   if (typeof window !== "undefined") return "";
+//   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+//   return "http://localhost:3000";
+// }
+
+// export function getUrl() {
+//   return getBaseUrl() + "/api/trpc";
+// }
+
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
-  return "http://localhost:3000";
+  if (process.env.NODE_ENV === 'production') {
+    return `https://${process.env.PRODUCTION_URL}`;
+  } else {
+    return "http://localhost:3000";
+  }
 }
 
 export function getUrl() {
